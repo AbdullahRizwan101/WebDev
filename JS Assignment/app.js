9d2f1188044cf21a260aed08bc99a429981fcd2a
AbdullahRizwan101 <60057481+AbdullahRizwan101@users.noreply.github.com>
2020-06-14 17:04:44 +0500

    Add files via upload

---
+++ JS Assignment/app.js
@@ -0,0 +1,210 @@
+// Chapter # 1
+// Task1
+// alert("Hello , Welcome to my website");
+
+// Task 2
+// alert("Error! , Please Enter valid password");
+
+// Task 3
+// alert("Welcome to JS Land \nHappy Coding!");
+
+// Task 4
+
+// alert("Welcome to JS Land..");
+// alert("Happy Coding!");
+
+// -----------------------------------------
+
+// Chapter # 2
+
+// Task 1
+// var username ;
+// Task 2
+// var myname = "Abdullah Rizwan";
+
+// Task 3
+// var message = "Hello world";
+// alert(message);
+
+// Task 4
+// var name = "John Doe";
+// var age = "15";
+// var cert = "Certifed Mobile Application Development";
+
+// alert(name);
+// alert(age+' years old');
+// alert(cert);
+
+// Task 5
+// alert("PIZZA"+'\n'+"PIZZ"+'\n'+"PIZ"+'\n'+"PI"+'\n'+'P');
+
+// Task 6
+// var email ="abc@gmail.com";
+// alert("My email is"+" "+email);
+
+// Task 7
+// var book = "A smarter way to learn Javascript";
+// alert("I am trying to learn from the book"+" "+book);
+
+// Task 8
+// document.write("Yah! I can write HTML content through Javascript");
+
+// Task 9
+
+// var stylish = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(stylish);
+// ------------------------------------------------
+// Chapter 3
+
+// Task 01
+// var age = 21;
+// alert("I am"+age+" years old");
+
+//Task 02
+// var visits = 14;
+// alert("You have visited this site "+visits);
+
+// Task 03
+// var birthyear = 1999;
+// document.write("My birth year is "+" "+birthyear+"<br>"+"Data type of my decalred variable is number");
+
+// Task 04
+// var v_name = "John Doe";
+// var product_title = "T-Shirt";
+// var qunatity = 5;
+// document.write(v_name.bold()+" ordered"+" "+qunatity+product_title.bold()+" on XYZ Clothing Store");
+
+// -----------------------------
+// Chapter 4
+
+// Task 01
+// var abc,xyz,def;
+
+// Task 02
+// Legal
+// var one,two,three,four,five;
+// Illegal
+// var if,else,for,while,try;
+
+// Task 03
+
+// document.write("<b>Rules for naming JS variables</b>");
+
+// document.write("<br><br>Variable names can only contain numbers,$ and _. For example :$my_1stVariable.");
+// document.write("<br>Variable must begin with a letter,$or _. For exmaple : $name,_name or name.");
+// document.write("<br>Variable names are case sensitive");
+// document.write("<br>Variable names should not be JS keywords");
+
+// --------------------------------
+
+// Chapter 5
+
+// Task 01 - 02
+// var one  = 3 ;
+// var two =  5 ;
+// var result = one + two;
+// document.write("Sum of "+one+" and "+two+" is : "+result);
+
+// var one  = 3 ;
+// var two =  5 ;
+// var result = one - two;
+// document.write("<br>Subtraction of "+one+" and "+two+" is : "+result);
+
+// var one  = 3 ;
+// var two =  5 ;
+// var result = one * two;
+// document.write("<br>Multiplaction of "+one+" and "+two+" is : "+result);
+
+// var one  = 3 ;
+// var two =  5 ;
+// var result = one / two;
+// document.write("<br>Division of "+one+" and "+two+" is : "+result);
+
+// Task 03
+
+
+// Task 04
+// var ticket = 600;
+// var ticket_cost = 5 * 600;
+// document.write("Total Cost to buy 5 tickets to a movie is : "+ticket_cost);
+
+// Task 05
+
+// var num = 6;
+
+// for(var i = 1 ; i <=10;i++)
+// {
+//     document.write(num+"x"+i+" = "+num*i+"<br>");
+// }
+
+// Task 06
+
+// Celsius to Fahrenhiet
+// var C = 25;
+
+// var F = (C*(9/5))+ 32;
+
+// document.write(C+"c"+" is "+F+"f");
+
+// Fahrenhiet to Celsius 
+
+// var F = 70;
+// var C = (F-32)*(5/9);
+// document.write(F+"f"+" is "+C+"c");
+
+
+// Task 07
+
+// var item_1 = 650;
+// var item_2 = 100;
+// var q_item_1 = 3;
+// var q_item_2 = 7;
+// var shipping_charges = 100;
+// var final_cost = item_1*q_item_1 + item_2*q_item_2 + shipping_charges;
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("<br> Price of Item 1 is "+item_1);
+// document.write("<br>Quanitiy of Item 1 is "+q_item_1);
+// document.write("<br>Price of Item 2 is "+item_2);
+// document.write("<br>Quantity of item 2 is "+q_item_2);
+// document.write("<br>Shipping Charges : "+shipping_charges);
+// document.write("<br><br> Total cost of your order is "+final_cost);
+
+// Task 08
+
+
+// var tot_marks = 980;
+// var obt_marks = 804;
+// var per = (obt_marks/tot_marks)*100;
+
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("<br><br>Total Marks : "+tot_marks);
+// document.write("<br>Marks Obtained : "+obt_marks);
+// document.write("<br>Percentage : "+per);
+
+// Task 09
+// var pkr_currency = 10*104.80 + 25*28;
+
+// document.write("<h1>Currency in PKR</h1>");
+// document.write("<br>Total Currency in PKR : ",pkr_currency);
+
+// Task 10
+
+// var  idk = 0 ;
+// idk = (idk + 5 ) * (10) / (2);
+// document.write(idk); 
+
+// Task 11
+// var curr_year = 2020;
+// var birth_year = 1999;
+// var age = curr_year - birth_year;
+
+// document.write("<h1>Age Calculator</h1>");
+// document.write("<br>Current Year : "+curr_year);
+// document.write("<br>Birth Year : "+birth_year);
+// document.write("<br>Your age is :"+age);
+
+
+
+
+
+
